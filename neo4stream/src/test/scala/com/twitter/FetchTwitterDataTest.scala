c48d30213fbf4ab17359ba2f737c5203303b5a4f
Anurag <anurag@knoldus.com>
2016-07-12 12:52:31 +0530

    Add method for fetching follower's list from the twitter

---
+++ neo4stream/src/test/scala/com/twitter/FetchTwitterDataTest.scala
@@ -0,0 +1,26 @@
+package com.twitter
+
+import org.scalatest.FunSuite
+
+class FetchTwitterDataTest extends FunSuite {
+
+   test("test twitter followers count") {
+      val totalCountOfFollower = FetchTwitterData.getTotalCountOfFollower("vhazrati")
+      assert(totalCountOfFollower.get == 316)
+    }
+
+    test("test twitter data") {
+      val getFollowers: Option[String] = FetchTwitterData.getFollowers("vhazrati")
+      assert(getFollowers.get == "Success")
+    }
+
+  test("get none when user not found") {
+    val totalCountOfFollower = FetchTwitterData.getTotalCountOfFollower("vsjakshshakjas")
+    assert(totalCountOfFollower.isEmpty)
+  }
+
+  test("get none in get follower when asked for follower's count ") {
+    val getFollowers: Option[String] = FetchTwitterData.getFollowers("vsjakshshakjas")
+    assert(getFollowers == Some("Unable to get follower's count"))
+  }
+}
